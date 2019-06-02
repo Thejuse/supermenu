31b773ac2c612e8e91bfefe591fd7ef269de6844
Julian Seidl <julian@julianseidl.at>
2019-06-02 16:53:07 +0200

    More functionality

---
+++ build/src/js/demo.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    $("#supermenu").supermenu();
+});
--- src/js/supermenu.js
+++ src/js/supermenu.js
@@ -7,3 +7,46 @@
  * 
  * License: MIT-License
  */
+
+function smClassAdderListItem(){
+    $('#supermenu li').each(function(){
+        $(this).addClass('sm-list-item');
+    });
+}
+function smClassAdderList(){
+    $('#supermenu ul').each(function(){
+        $(this).addClass('sm-list');
+        if($(this).parent().hasClass('sm-list-item')){
+            $(this).parent().addClass('sm-has-submenu');
+            $(this).addClass('sm-submenu');
+        }
+    });
+}
+function smClassAdderAnchor(){
+    $('#supermenu a').each(function(){
+        $(this).addClass('sm-list-item-anchor');
+    })
+}
+
+function smClassAdder(){
+    smClassAdderListItem();
+    smClassAdderList();
+    smClassAdderAnchor();
+}
+
+function smInit(menuID){
+    smClassAdder();
+    $(menuID).addClass('sm-init');
+}
+
+$.fn.supermenu = function(options){
+    smInit('#supermenu');
+    var smSettings = $.extend({}, $.fn.supermenu.defaults, options);
+
+};
+
+$.fn.supermenu.defaults = {
+    width: 70,
+    theme: 'light',
+    position: 'right'
+}
